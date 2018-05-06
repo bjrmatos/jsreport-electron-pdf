f6c7c5c8511cb413c1c75d136fa7805cb4e58112
BJR Matos <bjrmatos@gmail.com>
2018-05-06 13:21:02 -0500

    add options schema

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,7 +1,33 @@
 
+const electronSchema = {
+  type: 'object',
+  properties: {
+    strategy: {
+      type: 'string',
+      enum: ['dedicated-process', 'electron-ipc', 'electron-server'],
+      default: 'dedicated-process'
+    },
+    numberOfWorkers: { type: 'number' },
+    pingTimeout: { type: 'number' },
+    timeout: { type: 'number' },
+    portLeftBoundary: { type: 'number' },
+    portRightBoundary: { type: 'number' },
+    host: { type: 'string' },
+    chromeComandLineSwitches: { type: 'object' },
+    allowLocalFilesAccess: { type: 'boolean' },
+    maxLogEntrySize: { type: 'number' }
+  }
+};
+
 module.exports = {
   name: 'electron-pdf',
   main: 'lib/electron.js',
   dependencies: ['templates'],
+  optionsSchema: {
+    electron: { ...electronSchema },
+    extensions: {
+      'electron-pdf': { ...electronSchema }
+    }
+  },
   embeddedSupport: true
 };
